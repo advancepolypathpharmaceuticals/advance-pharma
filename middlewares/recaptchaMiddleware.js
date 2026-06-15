47c54bca99c024634effc3675abc099662b28f3f
advancepolypathpharmaceuticals <advancepolypathpharmaceuticals@gmail.com>
2026-06-15 22:49:37 +0530

    recaptchaMiddleware.js

--- middlewares/recaptchaMiddleware.js
+++ middlewares/recaptchaMiddleware.js
@@ -2,9 +2,17 @@ const verifyRecaptcha = require("../helpers/recaptcha");
 
 module.exports = async (req, res, next) => {
 
-  console.log("BODY =", req.body);
-  console.log("FILE =", req.file);
-  console.log("TOKEN =", req.body?.recaptchaToken);
+  const token = req.body.recaptchaToken;
+
+  if (!token) {
+    return res.status(400).send("reCAPTCHA token missing");
+  }
+
+  const verified = await verifyRecaptcha(token);
+
+  if (!verified) {
+    return res.status(400).send("reCAPTCHA verification failed");
+  }
 
   next();
 };
