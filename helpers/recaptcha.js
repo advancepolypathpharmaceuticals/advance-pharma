0a79892db63870ebf0ad4ef3f9cd0e5bae6f87db
advancepolypathpharmaceuticals <advancepolypathpharmaceuticals@gmail.com>
2026-06-15 23:33:53 +0530

    recaptcha.js

--- helpers/recaptcha.js
+++ helpers/recaptcha.js
@@ -1,26 +1,14 @@
-const axios = require("axios");
-
-async function verifyRecaptcha(token) {
-  try {
-    const response = await axios.post(
-      "https://www.google.com/recaptcha/api/siteverify",
-      null,
-      {
-        params: {
-          secret: process.env.RECAPTCHA_SECRET_KEY,
-          response: token,
-        },
-      }
-    );
-
-    console.log("RECAPTCHA RESPONSE:", response.data);
-
-    return response.data.success && response.data.score > 0.5;
-
-  } catch (err) {
-    console.log("reCAPTCHA Error:", err.message);
-    return false;
+const response = await axios.post(
+  "https://www.google.com/recaptcha/api/siteverify",
+  null,
+  {
+    params: {
+      secret: process.env.RECAPTCHA_SECRET_KEY,
+      response: token,
+    },
   }
-}
+);
 
-module.exports = verifyRecaptcha;
+console.log("RECAPTCHA RESPONSE:", response.data);
+
+return response.data.success && response.data.score > 0.5;
